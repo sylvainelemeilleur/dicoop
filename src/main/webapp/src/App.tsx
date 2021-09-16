a6351647c4643b22845adbe65ee5299920a924ea
fgd99 <fabriceguydominguez@gmail.com>
2021-09-16 19:11:27 +0200

    Minor UI modifications

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import SolutionSettingsForm from "./Solution/SolutionSettingsForm";
 import SolutionTable from "./Solution/SolutionTable";
 import ParticipantsTable from "./Participant/ParticipantsTable";
+import { Flex, FlexItem } from "@patternfly/react-core";
 
 function App() {
   const [isSolving, setIsSolving] = useState(false);
@@ -93,37 +94,45 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <SolutionSettingsForm />
-      {isSolving ? (
-        <button
-          className="pf-c-button pf-m-secondary"
-          type="button"
-          onClick={stopSolving}
-        >
-          Stop &nbsp;
-          <span
-            className="pf-c-spinner pf-m-sm"
-            role="progressbar"
-            aria-valuetext="Loading..."
+    <Flex direction={{ default: "column" }}>
+      <FlexItem>
+        <SolutionSettingsForm />
+      </FlexItem>
+      <FlexItem>
+        {isSolving ? (
+          <button
+            className="pf-c-button pf-m-secondary"
+            type="button"
+            onClick={stopSolving}
           >
-            <span className="pf-c-spinner__clipper"></span>
-            <span className="pf-c-spinner__lead-ball"></span>
-            <span className="pf-c-spinner__tail-ball"></span>
-          </span>
-        </button>
-      ) : (
-        <button
-          className="pf-c-button pf-m-primary"
-          type="button"
-          onClick={startSolving}
-        >
-          Solve
-        </button>
-      )}
-      <SolutionTable committeeSolution={committeeSolution} />
-      <ParticipantsTable persons={persons} />
-    </div>
+            Stop &nbsp;
+            <span
+              className="pf-c-spinner pf-m-sm"
+              role="progressbar"
+              aria-valuetext="Loading..."
+            >
+              <span className="pf-c-spinner__clipper"></span>
+              <span className="pf-c-spinner__lead-ball"></span>
+              <span className="pf-c-spinner__tail-ball"></span>
+            </span>
+          </button>
+        ) : (
+          <button
+            className="pf-c-button pf-m-primary"
+            type="button"
+            onClick={startSolving}
+          >
+            Solve
+          </button>
+        )}
+      </FlexItem>
+      <FlexItem>
+        <SolutionTable committeeSolution={committeeSolution} />
+      </FlexItem>
+      <FlexItem>
+        <ParticipantsTable persons={persons} />
+      </FlexItem>
+    </Flex>
   );
 }
 
--- src/main/webapp/src/Solution/SolutionSettingsForm.tsx
+++ src/main/webapp/src/Solution/SolutionSettingsForm.tsx
@@ -1,37 +1,130 @@
 import React, { useState } from "react";
-import { NumberInput } from "@patternfly/react-core";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Form,
+  FormGroup,
+  NumberInput,
+  Popover,
+} from "@patternfly/react-core";
+import HelpIcon from "@patternfly/react-icons/dist/esm/icons/help-icon";
 
 function SolutionSettingsForm(props: any) {
-  const [value, setValue] = useState(4);
+  const [nbProParticipants, setNbProParticipants] = useState(2);
+  const [nbNonProParticipants, setNbNonProParticipants] = useState(1);
   const min = 0;
   const max = 5;
-  const setControlledValue = (val: number) => {
-    val = Math.max(val, min);
-    val = Math.min(val, max);
-    setValue(val);
+  const setControlledValue = (val: number, valueSetter: any) => {
+    valueSetter(val);
   };
-  const onMinus = () => {
-    setControlledValue(value - 1);
+  const onMinus = (val: number, valueSetter: any) => {
+    return () => setControlledValue(val - 1, valueSetter);
   };
-  const onPlus = () => {
-    setControlledValue(value + 1);
+  const onPlus = (val: number, valueSetter: any) => {
+    return () => setControlledValue(val + 1, valueSetter);
   };
   return (
-    <div>
-      <NumberInput
-        value={value}
-        onMinus={onMinus}
-        onChange={(event) => {
-          const target = event.target as HTMLTextAreaElement;
-          setControlledValue(Number(target.value));
-        }}
-        onPlus={onPlus}
-        inputName="input"
-        inputAriaLabel="number input"
-        minusBtnAriaLabel="minus"
-        plusBtnAriaLabel="plus"
-      />
-    </div>
+    <Card cellPadding={30}>
+      <CardTitle>Settings</CardTitle>
+      <CardBody>
+        <Form isHorizontal>
+          <FormGroup
+            label="Number of professionals"
+            fieldId="nbProParticipants"
+            labelIcon={
+              <Popover
+                headerContent={
+                  <div>The number of professionals participants</div>
+                }
+                bodyContent={
+                  <div>
+                    Between {min} and {max}
+                  </div>
+                }
+              >
+                <button
+                  type="button"
+                  aria-label="More info for name field"
+                  onClick={(e) => e.preventDefault()}
+                  aria-describedby="simple-form-name-02"
+                  className="pf-c-form__group-label-help"
+                >
+                  <HelpIcon noVerticalAlign />
+                </button>
+              </Popover>
+            }
+            isInline
+          >
+            <NumberInput
+              id="nbProParticipants"
+              value={nbProParticipants}
+              onMinus={onMinus(nbProParticipants, setNbProParticipants)}
+              onChange={(event) => {
+                const target = event.target as HTMLTextAreaElement;
+                setControlledValue(Number(target.value), setNbProParticipants);
+              }}
+              onPlus={onPlus(nbProParticipants, setNbProParticipants)}
+              inputName="input"
+              inputAriaLabel="number input"
+              minusBtnAriaLabel="minus"
+              plusBtnAriaLabel="plus"
+              min={min}
+              max={max}
+              widthChars={2}
+            />
+          </FormGroup>
+          <FormGroup
+            label="Number of non-professionals"
+            fieldId="nbNonProParticipants"
+            labelIcon={
+              <Popover
+                headerContent={
+                  <div>The number of non-professionals participants</div>
+                }
+                bodyContent={
+                  <div>
+                    Between {min} and {max}
+                  </div>
+                }
+              >
+                <button
+                  type="button"
+                  aria-label="More info for name field"
+                  onClick={(e) => e.preventDefault()}
+                  aria-describedby="simple-form-name-02"
+                  className="pf-c-form__group-label-help"
+                >
+                  <HelpIcon noVerticalAlign />
+                </button>
+              </Popover>
+            }
+            isInline
+          >
+            <NumberInput
+              id="nbNonProParticipants"
+              value={nbNonProParticipants}
+              onMinus={onMinus(nbNonProParticipants, setNbNonProParticipants)}
+              onChange={(event) => {
+                const target = event.target as HTMLTextAreaElement;
+                setControlledValue(
+                  Number(target.value),
+                  setNbNonProParticipants
+                );
+              }}
+              onPlus={onPlus(nbNonProParticipants, setNbNonProParticipants)}
+              inputName="input"
+              inputAriaLabel="number input"
+              minusBtnAriaLabel="minus"
+              plusBtnAriaLabel="plus"
+              min={min}
+              max={max}
+              widthChars={2}
+            />
+          </FormGroup>
+        </Form>
+      </CardBody>
+    </Card>
   );
 }
 
